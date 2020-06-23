bc24ab50791cca465b872834ecf30a90297bf7c7
Sirawit yodjit <42561981+blackjokerrr@users.noreply.github.com>
2020-06-23 18:39:58 +0700

    Update Change mode

--- game.py
+++ game.py
@@ -5,11 +5,16 @@ from random import *
 
 class Windows:
     
+    
     frame = Tk()
     
+    frame.title('Calculatate Game')
+    
+    size = Frame(frame, width=300, height=40)
+    
+    
     def setFrame(self):
-        size = Frame(self.frame, width=400, height=400)
-        size.pack()
+        self.size.pack()
         
     
     def run(self):
@@ -35,6 +40,9 @@ class Question:
     label = Label(box_question.frame)
     random_question = ''
     listed_quesion = []
+    mode = 'Easy'
+    label_mode = Label(box_question.frame, text = mode)
+    label_mode.pack()
     
     def setQuestion(self, name_of_question = ''):
         
@@ -67,21 +75,49 @@ class Question:
         t.start()
         
         
-    def randomQuestion(self):
+    def randomQuestion(self, mode = ''):
         
-        self.listed_question = sample([1, 2, 3, 4, 5, 6, 7 , 8, 9 , 10], k=2)
         
+        if self.mode == 'Normal' or mode == 'Normal':
+            self.listed_question = sample([randrange(10, 100), randrange(10, 100)], k=2)
+            
+        elif self.mode == 'Hard' or mode == 'Hard':
+            self.listed_question = sample([randrange(100, 1000), randrange(100, 1000)], k=2)
+        
+        else:
+            self.listed_question = sample([1, 2, 3, 4, 5, 6, 7 , 8, 9 , 10], k=2)
+            
+            
         question = str(self.listed_question[0]) + ' + ' + str(self.listed_question[1])
-        
+            
         return question
+    
+    
         
+    def setMode(self, event):
+        
+        if event.char == 'n':
+            self.mode = 'Normal'
+            
+        elif event.char == 'h':
+            self.mode = 'Hard'
+            
+        else:
+            self.mode = 'Easy'
+            
+        self.label_mode.config(text = self.mode)
+        self.label_mode.pack()
         
     
     def show(self):
         
-        
+        self.box_question.size.bind_all('<e>', self.setMode)
+        self.box_question.size.bind_all('<n>', self.setMode)
+        self.box_question.size.bind_all('<h>', self.setMode)
+
         self.setQuestion()
         self.countingTime()
+    
     
     
     
@@ -89,32 +125,51 @@ class Answer:
     
     box_answer = Windows()
     object_question = Question()
-    txt = Text(box_answer.frame, width=20, height=2)
+    txt = Text(box_answer.frame, width=20, height=0)
     user = User()
     user.name = 'A'
     label_user = Label(box_answer.frame, text = 'User: ' + user.name + ' Score: ' + str(user.score))
     label_user.pack()
     
 
+    
         
-    def checkAnswer(self):
+    def checkAnswer(self, event):
         answer = self.txt.get(1.0, END)
         question = self.object_question.label.cget('text')
+        mode = ''
         
         check_answer = question.split(' ')
         
-        if (int(check_answer[0]) + int(check_answer[2])) == int(answer.strip('\n')):
-            self.object_question.setQuestion(self.object_question.randomQuestion())
+        if len(check_answer[0]) > 2:
+            mode = 'Hard'
+        elif len(check_answer[0]) > 1:
+            mode = 'Normal'
+        else:
+            mode = 'Easy'
+
+        
+        if (answer.strip('\n')).isdigit() and (int(check_answer[0]) + int(check_answer[2])) == int(answer.strip('\n')):
+            self.object_question.setQuestion(self.object_question.randomQuestion(mode))
             self.user.score += 1
             self.label_user.config(text = 'User: ' + self.user.name + ' Score: ' + str(self.user.score))
-
+            self.txt.delete(1.0, END)
+        elif (answer.strip('\n')).isdigit():
+            self.user.score -= 1
+            self.label_user.config(text = 'User: ' + self.user.name + ' Score: ' + str(self.user.score))
+            self.txt.delete(1.0, END)
+        else:
+            self.txt.delete(1.0, END)
+            
     
     def show(self):
         
-        btn = Button(self.box_answer.frame, text = 'Go', command = self.checkAnswer)
+        btn = Button(self.box_answer.frame, text = 'Go').place(x = 137, y = 110)
         
         self.txt.pack()
-        btn.pack()
+
+        self.box_answer.size.bind_all('<Return>', self.checkAnswer)
+        self.box_answer.size.pack()
         
         
         
